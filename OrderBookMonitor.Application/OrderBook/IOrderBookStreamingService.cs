41bbf3ee11d439c486e2fb39f7e2c17e75ed7520
kozakov95 <kozakov.k95@gmail.com>
2025-03-12 08:22:28 +0200

    feature-02: created IOrderBookStreamingService in application

---
+++ OrderBookMonitor.Application/OrderBook/IOrderBookStreamingService.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderBookMonitor.Application.OrderBook;
+
+public interface IOrderBookStreamingService
+{
+    Task StartStreamingAsync(CancellationToken cancellationToken);
+    
+    Task StopStreamingAsync(CancellationToken cancellationToken);
+}
---
+++ OrderBookMonitor/OrderBookMonitor/Modules/OrderBook/HostedServices/OrderBookStreamingBackgroundService.cs
@@ -0,0 +1,22 @@
+﻿using OrderBookMonitor.Application.OrderBook;
+
+namespace OrderBookMonitor.Modules.OrderBook.HostedServices;
+
+public class OrderBookStreamingBackgroundService: IHostedService
+{
+    private readonly IOrderBookStreamingService _streamingService;
+    public OrderBookStreamingBackgroundService(IOrderBookStreamingService streamingService)
+    {
+        _streamingService = streamingService;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _streamingService.StartStreamingAsync(cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _streamingService.StopStreamingAsync(cancellationToken);
+    }
+}
